c8202f1ac5bbfc4e635d35f54136d85cc73205b7
Raveen Dassanayake <raveenda00@gmail.com>
2023-12-06 21:51:32 +0530

    update signin api  route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -24,8 +24,12 @@ export const signin = async (req, res, next) => {
         const validPassword = bcryptjs.compareSync(password,validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-        res.cookie('access_token', token, { httpOnly: true}).status(200).json(validUser);
-        
+        const {password: pass, ...rest} = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true})
+            .status(200)
+            .json(rest);
+
     } catch (error) {
         next(error);
     }
